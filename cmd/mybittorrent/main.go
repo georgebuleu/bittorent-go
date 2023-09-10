6df16ff3c75f6fc16cd3589ab02410d76d721047
georgebuleu <georgebu44@gmail.com>
2023-09-10 22:23:30 +0300

    1st stage

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -42,7 +42,7 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	command := os.Args[2]
+	command := os.Args[1]
 
 	if command == "decode" {
 
