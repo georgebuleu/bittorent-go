45ea8198f39ae7816451ea9192b29d869eaf85a7
georgebuleu <georgebu44@gmail.com>
2023-09-10 22:05:25 +0300

    pass 1st stage

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -47,13 +47,11 @@ func main() {
 	if command == "decode" {
 
 		bencodedValue := os.Args[2]
-
 		decoded, err := decodeBencode(bencodedValue)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-
 		jsonOutput, _ := json.Marshal(decoded)
 		fmt.Println(string(jsonOutput))
 	} else {
