ffb0bfc3e37dfbb03a30f68b83f145420fa8e293
georgebuleu <georgebu44@gmail.com>
2023-09-11 17:08:32 +0300

    5th stagpe

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -6,7 +6,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
-	"strconv"
 	// bencode "github.com/jackpal/bencode-go" // Available if you need it!
 )
 
@@ -41,8 +40,8 @@ func main() {
 			fmt.Println(err)
 			return
 		}
-		fmt.Println("Tracker URL:" + info.URL)
-		fmt.Println("Length:" + strconv.FormatInt(info.length, 10))
+		fmt.Printf("Tracker URL: %s", info.URL)
+		//fmt.Println("Length:" + strconv.FormatInt(info.length, 10))
 		//fmt.Printf("%d", info.length)
 	} else {
 		fmt.Println("Unknown command: " + command)
